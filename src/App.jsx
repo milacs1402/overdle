8d695baf7dd421c380fe1a3554aa1859b820de56
LuisG0384 <luisgdm03@gmail.com>
2025-05-27 15:06:27 -0300

    POR FAVOR

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
-import listaHerois from "../src/assets/ListaHerois.json";
+import listaHerois from "public/ListaHerois.json";
 import "./styleApp.css";
 
 
